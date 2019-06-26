e194f95a184f0618a09d7926b3fcf9d30aee07bf
Roman Rybkin <rurybkin@gmail.com>
2019-06-26 15:17:53 +0300

    backend update

--- review-system-backend/review_backend.py
+++ review-system-backend/review_backend.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from textblob import TextBlob
 from random import randint
 import ast
@@ -9,17 +9,17 @@ app = Flask(__name__)
 def make_answer(polarity):
     if polarity > 0.0:
         answer = {
-            '1': "Thank you for your feedback! Have a nice day!",
-            '2': "We are glad that you liked everything! Thank you!",
-            '3': "Thank you so much for your review! We will be waiting to see you again!"
+            '1': ("Thank you for your feedback! Have a nice day!", "😊"),
+            '2': ("We are glad that you liked everything! Thank you!", "🤩"),
+            '3': ("Thank you so much for your review! We will be waiting to see you again!", "😍")
         }[str(randint(1,3))]
         print(answer)
         return answer
     else:
         answer = {
-            '1': "Thank you for your feedback! We will work on the quality of our services.",
-            '2': "Sorry to hear that! We will notify administator and take action. Thank you for your feedback!",
-            '3': "We are constantly improving and your feedback will help us to become better. Thank you so much!"
+            '1': ("****", "🤬"),
+            '2': ("Sorry to hear that! We will notify administator and take action. Thank you for your feedback!", "😔"),
+            '3': ("We are constantly improving and your feedback will help us to become better. Thank you so much!", "😟")
         }[str(randint(2,3))]
         print(answer)
         return answer
@@ -41,4 +41,4 @@ def process_review():
     print(request.headers['Content-Type'])
     if request.headers['Content-Type'] == 'application/json; charset=utf-8':
         print(request.json)
-        return analyze_review(ast.literal_eval(request.json))
+        return jsonify(analyze_review(request.json))
