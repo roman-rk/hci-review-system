7a733aa84bfb6347f03cd829e87b82072795c185
Roman Rybkin <rurybkin@gmail.com>
2019-06-29 16:55:39 +0300

    blahblah

--- flutter-front/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
+++ flutter-front/android/app/src/main/java/io/flutter/plugins/GeneratedPluginRegistrant.java
@@ -1,9 +1,9 @@
 package io.flutter.plugins;
 
 import io.flutter.plugin.common.PluginRegistry;
+import com.tundralabs.fluttertts.FlutterTtsPlugin;
 import com.ly.permission.PermissionPlugin;
 import bz.rxla.flutter.speechrecognition.SpeechRecognitionPlugin;
-import com.blounty.tts.TtsPlugin;
 
 /**
  * Generated file. Do not edit.
@@ -13,9 +13,9 @@ public final class GeneratedPluginRegistrant {
     if (alreadyRegisteredWith(registry)) {
       return;
     }
+    FlutterTtsPlugin.registerWith(registry.registrarFor("com.tundralabs.fluttertts.FlutterTtsPlugin"));
     PermissionPlugin.registerWith(registry.registrarFor("com.ly.permission.PermissionPlugin"));
     SpeechRecognitionPlugin.registerWith(registry.registrarFor("bz.rxla.flutter.speechrecognition.SpeechRecognitionPlugin"));
-    TtsPlugin.registerWith(registry.registrarFor("com.blounty.tts.TtsPlugin"));
   }
 
   private static boolean alreadyRegisteredWith(PluginRegistry registry) {
--- flutter-front/ios/Runner/GeneratedPluginRegistrant.m
+++ flutter-front/ios/Runner/GeneratedPluginRegistrant.m
@@ -3,16 +3,16 @@
 //
 
 #import "GeneratedPluginRegistrant.h"
+#import <flutter_tts/FlutterTtsPlugin.h>
 #import <permission/PermissionPlugin.h>
 #import <speech_recognition/SpeechRecognitionPlugin.h>
-#import <tts/TtsPlugin.h>
 
 @implementation GeneratedPluginRegistrant
 
 + (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FlutterTtsPlugin registerWithRegistrar:[registry registrarForPlugin:@"FlutterTtsPlugin"]];
   [PermissionPlugin registerWithRegistrar:[registry registrarForPlugin:@"PermissionPlugin"]];
   [SpeechRecognitionPlugin registerWithRegistrar:[registry registrarForPlugin:@"SpeechRecognitionPlugin"]];
-  [TtsPlugin registerWithRegistrar:[registry registrarForPlugin:@"TtsPlugin"]];
 }
 
 @end
